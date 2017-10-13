e30db7d5ccce580fd994fb00bc1aff8e84186317
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2017-10-12 17:31:48 -0700

    4 linux

--- env_install.sh
+++ env_install.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/JarrodCTaylor/dotfiles.git ~/dotfiles
 cd ~/dotfiles/install-scripts
-bash OSX/install-packages.sh
-bash OSX/create-symlinks.sh
+bash Linux/install-packages.sh
+bash Linux/create-symlinks.sh
 cd ~/dotfiles/custom-configs
 git clone https://github.com/jtestard/dotfiles .
 source ~/.zshrc
